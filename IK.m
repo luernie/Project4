dc506ec9548f799f39437fb49dadf06822d6aa8f
luernie <erniel1@uci.edu>
2025-02-28 13:24:54 -0800

    fixed up the Jacobian to be correct, fixed the FK to represent all three frames correctly, now working on the evec/eval for loop

--- IK.m
+++ IK.m
@@ -13,34 +13,4 @@ function [t1, t2] = IK(Px, Py, L1, L2)
     st1 = -L2 * s2 * Px + (L1 + L2 * c2) * Py; %denominator cancels out
     ct1 = (L1 + L2 * c2) * Px + L2 * s2 * Py;
     t1 = atan2(st1, ct1);
-    disp("I AM INSIDE THIS FUNCTION")
-end
-
-%% FK Function
-function [T, Tfinal] = FK(alpha, a, d, theta) % Calculated FK (263A Final Exam code)
-    %syms d1 a2 a3
-    % Initialization of the transformation from the base to the end effector
-    T0_ee = eye(4);
-    T = struct(); % Initialize structure array for T
-    
-    for i = 1:length(alpha)
-        fprintf('T (%d) to (%d)\n', i-1, i)
-        % Calculate each transformation matrix and store in structure array
-        T(i).matrix = TF(a(i), alpha(i), d(i), theta(i)); % Assuming TF is defined elsewhere
-        pretty(simplify(T(i).matrix))
-        T0_ee = T0_ee * T(i).matrix; % Sequential multiplications
-    end
-    
-    % Simplify the final transformation
-    Tfinal = simplify(T0_ee, 'Steps', 10);
-    
-    % Display and return Tfinal
-    pretty(Tfinal);
-end
-%% Subset FK Function
-function T = TF(a,alpha,d,theta) % This function is used in FK
-    T = [cos(theta) -sin(theta) 0 a
-    sin(theta)*cos(alpha) cos(theta)*cos(alpha) -sin(alpha) -sin(alpha)*d
-    sin(theta)*sin(alpha) cos(theta)*sin(alpha) cos(alpha) cos(alpha)*d
-    0 0 0 1];
 end
--- Project4.m
+++ Project4.m
@@ -2,7 +2,6 @@
 % Can change the variable under the FK function where we define simplify
 % steps
 clear all; close all; clc;
-
 %% Initialize Robot
 %% Robot Parameters
 % Setup
@@ -22,8 +21,9 @@ d   =   [0   0 0];
 theta = [t1  t2 0];
 
 % Define the robot with Robot Toolbox
-L(1) = Link('revolute','d',0,'a',L1,'alpha',0,'modified');
-L(2) = Link('revolute','d',0,'a',L2,'alpha',0,'modified');
+L(1) = Link('revolute','d',0,'a',0,'alpha',0,'modified'); % base frame
+L(2) = Link('revolute','d',0,'a',L1,'alpha',0,'modified'); % frame 1
+L(3) = Link('revolute','d',0,'a',L2,'alpha',0,'modified'); % frame 2
 Robot = SerialLink(L, 'name', '2R SCARA Arm') % Combine Link objects together to form a Robot Object
 %% FK Calculations
 [T, Tfinal] = FK(alpha, a, d, theta);
@@ -31,23 +31,22 @@ Robot = SerialLink(L, 'name', '2R SCARA Arm') % Combine Link objects together to
 x = L2*cos(t1+t2)+L1*cos(t1);
 y = L2*sin(t1+t2)+L1*sin(t1);
 simplify(sqrt(x^2+y^2), 'Steps', 10)
-
 %% Jacobian Calculation Explicit Method
 % Forward Kinematics - Get T matrices from T structure
 T01 = T(1).matrix;
 T12 = T(2).matrix;
-%T23 = T(3).matrix;
+T23 = T(3).matrix;
 T02 = simplify(T01 * T12);
-%T03 = simplify(T01 * T12 * T23);
+T03 = simplify(T01 * T12 * T23);
 P01 = T01(1:3, 4);
 P02 = T02(1:3, 4);
-%P03 = T03(1:3, 4);
-Pe = T02(1:3,4); %BUG: This should not be Tfinal but T02 cause we don't care about that
+% P03 = T03(1:3, 4);
+Pe = T03(1:3,4); %BUG: This should not be Tfinal but T02 cause we don't care about that
 % Direct Differentiation
 % Follow the similar precedure with previous example [3 col of the transformation matrix]
 Z01 = T01(1:3, 3); % z-axis of frame 1 (Joint 1 axis)
 Z02 = T02(1:3, 3); % z-axis of frame 2 (Joint 1 axis)
-%Z03 = T03(1:3, 3); % z-axis of frame 3 (Joint 1 axis)
+% Z03 = T03(1:3, 3); % z-axis of frame 3 (Joint 1 axis)
 %%
 J1 = [cross(Z01,(Pe-P01)); Z01]; % Revolute Joint Equation
 J2 = [cross(Z02,(Pe-P02)); Z02]; 
@@ -60,6 +59,8 @@ J_DD = simplify([J1, J2])
 
 % BUG: We only care about the joint velocities and not the end effector value.
 
+%% Scratch New
+
 %% Eigenvalues and eigenvectors of the JJt
 J_Jtranspose = simplify(J_DD * J_DD.');
 
@@ -67,48 +68,50 @@ J_Jtranspose = simplify(J_DD * J_DD.');
 [eVec, eVal] = eig(J_Jtranspose);
 
 % TODO: Graph the ellipses, first calculate the IK in btween though
-% get the joint angles for different 
-% check if the jacobian is singular
 X = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1];
-X = [0];
 Y = 0;
-
+% Robot Configurations
+%L1=0.25; L2=0.75; % Configuration 1
 L1=0.5; L2=0.5; % Configuration 2
+%L1=0.75; L2=0.25; % Configuration 3
 
-for i = 1:length(X)
-    [t1, t2] = IK(Px, Py, Pz, L1, L2);
+% Redefine the robot
+L(1) = Link('revolute','d',0,'a',0,'alpha',0,'modified'); % base frame
+L(2) = Link('revolute','d',0,'a',L1,'alpha',0,'modified'); % frame 1
+L(3) = Link('revolute','d',0,'a',L2,'alpha',0,'modified'); % frame 2
+Robot = SerialLink(L, 'name', '2R SCARA Arm') % Combine Link objects together to form a Robot Object
+
+syms t1 t2; %TODO: here is the stuff for 
+symbolic_expr = t1^2 + t2^2;
+t1_value = 1.5;  % Numeric value for t1
+t2_value = 0.7;  % Numeric value for t2
+numeric_expr = subs(symbolic_expr, {t1, t2}, {t1_value, t2_value});
+disp(['Result of substituting t1 = ', num2str(t1_value), ' and t2 = ', num2str(t2_value)]);
+disp(['Numeric Expression: ', char(numeric_expr)]);
+
+L_2(2) = Link('revolute','d',0,'a',L1,'alpha',0,'modified'); % frame 1
+L_2(3) = Link('revolute','d',0,'a',L2,'alpha',0,'modified'); % frame 2
+
+% TODO: For loop for each configuration, for each x value
+
+
+
+for i = 1:5 %1:length(X)
+    % Calculate Ik for each X,Y combo
+    [t1, t2] = IK(X(i), Y, L1, L2);
+
+    % Graph that robot position
     figure()
-    Robot.plot([t1 t2], 'workspace', [-1 1 -1 1 0 2]) % radians express
+    Robot.plot([t1 t2 0], 'workspace', [-1.5 1.5 -1.5 1.5 -.5 1]) % radians express
+    % FIXME: SO t1 is actually controlling t2 and t2 just changes the EE orientation
+    % Bug: So the first robot link is not appearing for some reason here
+    
+    % Add onto the ellipse
+    % Robot.vellipse([t1 t2], 'fillcolor','b', 'edgecolor','w',alpha',0.5);
+    % Robot.fellipse([t1 t2], 'fillcolor','r', 'edgecolor','w',alpha',0.5);
+    % BUG: Not working here for the ellipse, why Jacobian bad?
     disp("hello")
-    % TODO: Plot the robot in that config
     % TODO: get both ellipses on to the graph
     % TODO: Graph the matrix stuff for each
     % TODO: If not able to then skip and say its singular
-    figure()
-    Robot.plot([t1 t2], 'workspace', [-1 1 -1 1 0 2]) % radians express
 end
-
-
-%% Plot robots: Robotname.plot()
-
-%L1=0.5; L2=0.5; % Configuration 2
-
-%testX = .350;
-%testY = 0;
-%estZ = 0;
-%[testt1, testt2, testt3, testd4] = ScaraIK(testX, testY, testZ, 0, d1, a2, a3);
-
-%figure()
-%Robot.plot([t1 t2], 'workspace', [-1 1 -1 1 0 2]) % radians express
-
-% Example usage of IK function
-Px = 1;   % Replace with actual x-coordinate
-Py = 2;   % Replace with actual y-coordinate
-Pz = 0;   % Replace with actual z-coordinate
-L1 = 3;   % Replace with actual link length L1
-L2 = 2;   % Replace with actual link length L2
-
-[t1, t2] = IK(Px, Py, L1, L2);
-% Display results
-disp(['t1 = ', num2str(t1)]);
-disp(['t2 = ', num2str(t2)]);
